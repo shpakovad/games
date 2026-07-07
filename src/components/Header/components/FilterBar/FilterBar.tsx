56b4d82c1fb5c769db4c78f2a2fde848ca154c6e
Dasha <shpakovad@gmail.com>
2026-07-07 19:58:06 +0300

    Added filter bar UI

--- src/components/Header/components/FilterBar/FilterBar.tsx
+++ src/components/Header/components/FilterBar/FilterBar.tsx
@@ -1,7 +1,22 @@
+import chevronDown from "@/assets/icons/chevron_down.svg";
+
 import "./styles.scss";
 
 const FilterBar = () => {
-  return <div className="filter-bar">FilterBar</div>;
+  return (
+    <div className="filter-bar">
+      <label>Game Type</label>
+      <div className="filter-bar__select-wrapper">
+        <select value="" onChange={() => {}}>
+          <option value="option1">All</option>
+          <option value="option1">Variant 1</option>
+          <option value="option2">Variant 2</option>
+          <option value="option3">Variant 3</option>
+        </select>
+        <img src={chevronDown} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default FilterBar;
